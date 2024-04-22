eade1116bdfdf0d2c102085a941a11422db5c3df
Lucas Bohrer <lucasbmc13@outlook.com>
2024-04-22 18:13:48 -0300

    Criando o padrão MVC

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [CarController::class, 'index']);
+Route::get('/dashboard', [CarController::class, 'dashboard'])->middleware(['auth', 'verified']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
